bd338e9f205e3716d3799cdc6c7bfa6f942fc7ff
Ben <90252330+benjamindataiads@users.noreply.github.com>
2024-12-23 13:09:22 +0000

    Assistant checkpoint: Fix EditRule component import and routing
    Assistant generated file changes:
    - client/src/App.tsx: Add EditRule component import and fix route
    Add EditRule component import and fix route
    Fix route path for edit rule
    
    ---
    
    User prompt:
    
    502

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { AuditResults } from "./pages/AuditResults";
+import { RuleLibrary } from "./pages/RuleLibrary";
+import { CreateRule } from "./pages/CreateRule";
+import { EditRule } from "./pages/EditRule";
+import { AuditHistory } from "./pages/AuditHistory";
 
-<old_str>
-          <Route path="/rule-library" component={RuleLibrary} />
-          <Route path="/create-rule" component={CreateRule} />
-          <Route path="/audit/:id" component={AuditResults} />
-</old_str>
-<new_str>
+function App() {
+  return (
+    <div className="App">
+      <main className="container mx-auto py-6">
+        <Switch>
+          <Route path="/" component={Home} />
           <Route path="/rule-library" component={RuleLibrary} />
           <Route path="/create-rule" component={CreateRule} />
           <Route path="/rule-edit" component={EditRule} />
           <Route path="/audit/:id" component={AuditResults} />
-</new_str>
+          <Route path="/audits" component={AuditHistory} />
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
