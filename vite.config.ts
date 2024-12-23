f40c7e7b9240f60d71c1c4bcae193755165aaa8d
Ben <90252330+benjamindataiads@users.noreply.github.com>
2024-12-23 11:48:42 +0000

    Restored to '4b19a1241aef2dd5e0af68f328fcbfae05a6e59f'
    Replit-Restored-To:4b19a1241aef2dd5e0af68f328fcbfae05a6e59f

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
+import path, { dirname } from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443,
-    }
-  },
+  plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      "@db": path.resolve(__dirname, "./db"),
+      "@db": path.resolve(__dirname, "db"),
+      "@": path.resolve(__dirname, "client", "src"),
     },
   },
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist/public"),
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': 'http://0.0.0.0:3003'
+    }
+  }
 });
